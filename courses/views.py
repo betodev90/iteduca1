f7fc476c1f9ef11da9bcff48cccfb730b021d1ec
betodev90 <betodeveloper90@hotmail.com>
2018-04-22 00:12:08 -0500

    Agregado paginacion /por hacer completar con busqueda

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Curso
 
@@ -17,5 +18,15 @@ def index(request):
 @login_required(login_url='/login')
 def lista_cursos(request):
     """"""
-    cursos = Curso.objects.all()
+    q = request.GET.get('q', '')
+    querys = (Q(titulo__icontains=q) | Q(asignatura__titulo__icontains=q))
+    cursos = Curso.objects.filter(querys)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(cursos, 5)
+    try:
+        cursos = paginator.page(page)
+    except PageNotAnInteger:
+        cursos = paginator.page(1)
+    except EmptyPage:
+        cursos = paginator.page(paginator.num_pages)
     return render(request, 'list_cursos.html', context={'cursos': cursos})
